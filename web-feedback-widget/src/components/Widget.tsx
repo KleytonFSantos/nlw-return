86e869b9ce5882214b7b8923db300756f4d2d36b
KleytonFSantos <akatsukipb12@gmail.com>
2022-05-15 15:28:38 -0300

    first commit

---
+++ web-feedback-widget/src/components/Widget.tsx
@@ -0,0 +1,25 @@
+import { ChatTeardropDots } from "phosphor-react";
+import { Popover } from '@headlessui/react'
+
+function Widget() {
+  
+
+
+  return (
+
+    <Popover className="absolute bottom-5 right-5">
+        <Popover.Panel>Hello mermao</Popover.Panel>
+
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 flex items-center group">
+        <ChatTeardropDots className=" text-white w-6 h-6" />
+
+        <span className="text-white max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  );
+}
+
+export default Widget;
